5f06326a167c72654ec44cc102ff63b089ce8a09
shobhit123 <shobhittiwari859@gmail.com>
2025-09-21 15:41:49 +0530

    cloning..

--- src/javaCode/fffuhj.java
+++ src/javaCode/fffuhj.java
@@ -1,5 +1,16 @@
 package javaCode;
 
 public class fffuhj {
-
+/**
+ * in my current project we are using jira for bug tracking.wenever we find defect 
+ * we loging into jira with details like steps to reproduce, severity, priority 
+ * and screenShots. then it gets assigned to developer and we can track the bug status
+ * untill the status got closed.
+ * 
+ * 
+ * 
+ */
+	public static void main(String[] args) {
+		System.err.println("shobhit");
+	}
 }
